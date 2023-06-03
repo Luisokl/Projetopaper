541b56943cca562ad1141d055b262d90f3ec46d9
Luis Fernando <100451426+Luisokl@users.noreply.github.com>
2023-06-02 21:54:02 -0300

    Mudanças de tela no formulário

---
+++ script/login.js
@@ -0,0 +1,13 @@
+function loginShow() {
+
+    document.getElementById('cadastroForm').classList.add('hidden');
+    document.getElementById('loginForm').classList.remove('hidden');
+
+}
+
+function cadastroShow() {
+
+    document.getElementById('loginForm').classList.add('hidden');
+    document.getElementById('cadastroForm').classList.remove('hidden');
+}
+
